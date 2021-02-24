b2538bcde0f2b2da707233bed3beb667c396225d
mayhem929 <adrianjf2002@gmail.com>
2021-02-24 13:29:06 +0100

    versión alpha 1.0

--- OpenMeet.py
+++ OpenMeet.py
@@ -8,12 +8,16 @@ from datetime import datetime
 auth_user = '1'
 
 mp_teoria = 'https://meet.google.com/mme-zppa-ptf?pli=1&authuser=' + auth_user
-mp_pract = 'https://meet.google.com/tiu-gzpp-gfq?pli=1&authuser=' + auth_user
-fs = 'https://meet.google.com/brs-jtop-ivv?pli=1&authuser=' + auth_user
-geo = 'https://meet.google.com/fku-gqfb-ybw?pli=1&authuser=' + auth_user
-calc = 'https://meet.google.com/sgo-fqpd-hko?pli=1&authuser=' + auth_user
-edip = 'https://youtu.be/bxqLsrlakK8'
-mn = 'https://meet.google.com/oej-ooub-tuq?pli=1&authuser=' + auth_user
+mp_pract  = 'https://meet.google.com/tiu-gzpp-gfq?pli=1&authuser=' + auth_user
+fs        = 'https://meet.google.com/brs-jtop-ivv?pli=1&authuser=' + auth_user
+geo       = 'https://meet.google.com/fku-gqfb-ybw?pli=1&authuser=' + auth_user
+calc      = 'https://meet.google.com/jfy-mqvy-ceb?pli=1&authuser=' + auth_user
+edip1     = 'https://meet.google.com/bqh-etiv-tta?pli=1&authuser=' + auth_user
+edip2     = 'https://meet.google.com/wmb-uufq-kdy?pli=1&authuser=' + auth_user
+edip3     = 'https://meet.google.com/ovy-thqp-gtg?pli=1&authuser=' + auth_user
+mn        = 'https://meet.google.com/oej-ooub-tuq?pli=1&authuser=' + auth_user
+rr        = 'https://youtu.be/bxqLsrlakK8'
+
 
 ##########################################################################
 # Funcion para abrir la pestaña
@@ -21,48 +25,59 @@ mn = 'https://meet.google.com/oej-ooub-tuq?pli=1&authuser=' + auth_user
 def open_url(url):
     webbrowser.open_new_tab(url)
 
+
 ##########################################################################
 # Encontramos la hora y dia actual
 
 date = datetime
 now = date.now()
-day = now.weekday()
-day_name = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-
 hora = now.hour
 minuto = now.minute
+day = 3
+day_name = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+rkrl = True
 
 ##########################################################################
 # Lunes
 
 hora_ref = 17
-min_ref = 45
 
 if day_name[day] == 'Monday':
 
-    if hora < hora_ref:
-        webbrowser.open_new_tab(mp_teoria)
+    if hora_ref - 2 < hora < hora_ref:
+        open_url(mp_teoria)
+    elif hora_ref < hora < hora_ref + 2:
+        open_url(mp_pract)
     else:
-        webbrowser.open_new_tab(mp_pract)
+        open_url(rr)
 
 ##########################################################################
 # Martes
 
+hora_ref = 17
+
 if day_name[day] == 'Tuesday':
 
-    open_url(fs)
+    if hora_ref - 2 < hora < hora_ref + 2:
+        open_url(fs)
+    else:
+        open_url(rr)
 
 ##########################################################################
 # Miércoles
 
 hora_ref = 16
+min_ref = 45
 
 if day_name[day] == 'Wednesday':
-    for asig in [calc, mn, mn, edip, geo]:
+    for asig in [calc, mn, mn, edip1, geo]:
         if (hora == hora_ref - 1 and minuto >= min_ref) or \
                 (hora == hora_ref and minuto < min_ref):
             open_url(asig)
-            hora_ref += 1
+            rkrl = False
+        hora_ref += 1
+    if rkrl: open_url(rr)
 
 ##########################################################################
 # Jueves
@@ -70,11 +85,13 @@ if day_name[day] == 'Wednesday':
 hora_ref = 15
 
 if day_name[day] == 'Thursday':
-    for asig in [calc, calc, mn, edip, edip, geo]:
+    for asig in [calc, calc, mn, edip2, edip2, geo]:
         if (hora == hora_ref - 1 and minuto >= min_ref) or \
                 (hora == hora_ref and minuto < min_ref):
             open_url(asig)
-            hora_ref += 1
+            rkrl = False
+        hora_ref += 1
+    if rkrl: open_url(rr)
 
 ##########################################################################
 # Viernes
@@ -82,10 +99,20 @@ if day_name[day] == 'Thursday':
 hora_ref = 16
 
 if day_name[day] == 'Friday':
-    for asig in [calc, mn, edip, geo, geo]:
+    for asig in [calc, mn, edip3, geo, geo]:
         if (hora == hora_ref - 1 and minuto >= min_ref) or \
                 (hora == hora_ref and minuto < min_ref):
             open_url(asig)
-            hora_ref += 1
+            rkrl = False
+        hora_ref += 1
+    if rkrl: open_url(rr)
 
 ##########################################################################
+################## ᕦ( ͡° ͜ʖ ͡°)ᕤ EL FINDE ¯\_(ツ)_/¯ ######################
+##########################################################################
+
+if day_name[day] == 'Saturday':
+    open_url('https://www.youtube.com/watch?v=MpqGPJz5aQY')
+
+if day_name[day] == 'Sunday':
+    open_url('https://www.youtube.com/watch?v=Vz1-R3UKVuw')
